10c24f5399e3e60843117b8b757b71b33c611b6b
Kirill Mikhailin <mihailinkirill@gmail.com>
2023-02-06 17:09:14 +0300

    plus proxy

--- chromedriver/main.py
+++ chromedriver/main.py
@@ -1,15 +1,39 @@
-from selenium import webdriver
+#from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
+#from seleniumwire import webdriver
 import  time
+import random
+from fake_useragent import UserAgent
 
-#options
+user_agents_list = [
+    "Mozilla/5.0 (Macintosh; Intel Mac OS X 13_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36",
+    "Mozilla/5.0 (Windows NT 10.0; Trident/7.0; rv:11.0) like Gecko",
+    "Mozilla/5.0 (Macintosh; Intel Mac OS X 13_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 YaBrowser/23.1.0 Yowser/2.5 Safari/537.36",
+    "Mozilla/5.0 (iPhone; CPU iPhone OS 16_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) CriOS/109.0.5414.112 Mobile/15E148 Safari/604.1"
+]
+
+useragent = UserAgent()
+
+#set options
 options = webdriver.ChromeOptions()
 options.add_argument("--disable-blink-features=AutomationControlled")
 options.add_experimental_option("excludeSwitches", ["enable-logging"])
 #options.add_experimental_option("excludeSwitches", ["enable-automation"])
 # options.add_experimental_option('useAutomationExtension', False)
-options.add_argument("user-agent=HelloMyWorld:)")
 
+# options.add_argument("user-agent=HelloMyWorld:)")
+# options.add_argument("user-agent=Mozilla/5.0 (Linux; Android 10; SM-G960U) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.5414.117 Mobile Safari/537.36")
+# options.add_argument(f"user-agent={random.choice(user_agents_list)}") 
+options.add_argument(f"user-agent={useragent.random}") 
+
+#set proxy
+options.add_argument("--proxy-server=138.128.90.54:8000")
+
+proxy_options = {
+    "proxy" : {
+        "https": f"http://{login}:{password}@138.128.90.54:8000"
+    }
+}
 
 
 s = Service(executable_path=r"D:\python_today_practice\0.selenium\chromedriver\chromedriver.exe")
@@ -33,11 +57,15 @@ try:
     # driver.get('https://anycoindirect.eu')
     
     #user-agent
-    driver.get('https://www.whatismybrowser.com/detect/what-is-my-user-agent')
-    
-    time.sleep(10)
+    # driver.get('https://www.whatismybrowser.com/detect/what-is-my-user-agent')
+    # time.sleep(5)
     # driver.get(url=url2)
     # time.sleep(10)
+    
+    driver.get("https://2ip.ru")
+    time.sleep(5)
+    
+    
 except Exception as ex:
     print(ex)
 finally:
